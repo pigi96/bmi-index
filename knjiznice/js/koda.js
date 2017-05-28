91ade7c835ee17e8a54952fa7548e98b30a07e77
Jan Lovšin <jl3007@student.uni-lj.si>
2017-05-28 16:41:07 +0000

    Final

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -5,6 +5,11 @@ var queryUrl = baseUrl + '/query';
 var username = "ois.seminar";
 var password = "ois4fri";
 
+var valueEHRID = ["", "", ""];
+
+var BMI = 0;
+
+var first_open = true;
 
 /**
  * Prijava v sistem z privzetim uporabnikom za predmet OIS in pridobitev
@@ -30,13 +35,231 @@ function getSessionId() {
  * @param stPacienta zaporedna številka pacienta (1, 2 ali 3)
  * @return ehrId generiranega pacienta
  */
+function pridobiPodatke(){
+	var sessionId=getSessionId();
+	
+  ehrId = $("#EHR-ID").val();
+
+  if(!ehrId || ehrId.trim().length == 0){
+      $("#obvestilo-id").append("<span class='obvestilo " +
+                      "label-danger fade-in'>EHR polje je prazno!<br></span><br><br>");
+   } else {
+	$.ajax({
+    	url: baseUrl + "/demographics/ehr/" + ehrId + "/party",
+    	type: 'GET',
+    	headers: {
+        	"Ehr-Session": sessionId
+    	},
+    	success: function (data) {
+        	$("#imeU").val(data.party.firstNames);
+        	$("#priimekU").val(data.party.lastNames);
+    	}
+	});
+
+	$.ajax({
+    	url: baseUrl + "/view/" + ehrId + "/weight",
+    	type: 'GET',
+    	headers: {
+        	"Ehr-Session": sessionId
+    	},
+    	success: function (teza) {
+                $.ajax({
+                    url: baseUrl + "/view/" + ehrId + "/height",
+                    type: 'GET',
+                    headers: {
+                        "Ehr-Session": sessionId
+                    },
+                    success: function (visina) {
+                        $("#visinaU").val(visina[0].height);
+                        $("#tezaU").val(teza[0].weight);
+
+						BMI = parseFloat(teza[0].weight) / (parseFloat(visina[0].height) * parseFloat(visina[0].height) / 10000);
+						BMI = BMI.toFixed(2);
+						
+						narisiGraf(BMI);
+                    },
+	            error: function(err) {
+	            	$("#obvestilo-id").append("<span class='obvestilo label " +
+                "label-danger fade-in'>Napaka '" +
+                JSON.parse(err.responseText).userMessage + "'!</span><br>");
+                return;
+	            }
+                });
+    		},
+	            error: function(err) {
+	            	$("#obvestilo-id").append("<span class='obvestilo label " +
+                "label-danger fade-in'>Napaka '" +
+                JSON.parse(err.responseText).userMessage + "'!</span><br>");
+                return;
+	            }
+		});
+	}
+}
+ 
+function generirajPodatke1to3() {
+    generirajPodatke(0);
+    generirajPodatke(1);
+    generirajPodatke(2);
+}
+
+function prikazi(stPacienta) {
+	if (stPacienta == 0) {
+			if (valueEHRID[0] != "") {
+				document.getElementById('EHR-ID').value=valueEHRID[0];
+			} else {
+				$("#obvestilo-id").append("<span class='obvestilo " +
+                      "label-danger fade-in'>EHR se ni generiran!<br></span><br><br>");
+			}
+	} else if (stPacienta == 1) {
+			if (valueEHRID[1] != "") {
+				//$('EHR-ID').value=valueEHRID[1];
+				document.getElementById('EHR-ID').value=valueEHRID[1];
+			} else {
+				$("#obvestilo-id").append("<span class='obvestilo " +
+                      "label-danger fade-in'>EHR se ni generiran!<br></span><br><br>");
+			}
+	} else if (stPacienta == 2) {
+			if (valueEHRID[2] != "") {
+				//$('EHR-ID').value=valueEHRID[2];
+				document.getElementById('EHR-ID').value=valueEHRID[2];
+			} else {
+				$("#obvestilo-id").append("<span class='obvestilo " +
+                      "label-danger fade-in'>EHR se ni generiran!<br></span><br><br>");
+			}
+	}
+}
+
 function generirajPodatke(stPacienta) {
-  ehrId = "";
-
-  // TODO: Potrebno implementirati
-
+    var sessionId = getSessionId();
+   
+    var ime = "";
+	var priimek = "";
+	var ehrId = "";
+    
+    if (stPacienta == 0) {
+        ime = "Frenk";
+        priimek = "Kafe";
+    } else if (stPacienta == 1) {
+        ime = "Angelca";
+        priimek = "Buteljko";
+    } else if (stPacienta == 2) {
+        ime = "Gatis";
+        priimek = "Kandis";
+    }
+   
+    $.ajaxSetup({
+	    headers: {"Ehr-Session": sessionId}
+	});
+	$.ajax({
+	    url: baseUrl + "/ehr",
+	    type: 'POST',
+	    success: function (data) {
+	        var ehrId = data.ehrId;
+	        var partyData = {
+	            firstNames: ime,
+	            lastNames: priimek,
+	            partyAdditionalInfo: [
+	            	{key: "ehrId", value: ehrId},
+	            	]
+	        };
+	        $.ajax({
+	            url: baseUrl + "/demographics/party",
+	            type: 'POST',
+	            contentType: 'application/json',
+	            data: JSON.stringify(partyData),
+	            success: function (party) {
+	                if (party.action == 'CREATE') {
+	                    $("#obvestilo-id").append("<span class='obvestilo " +
+                      "label label-success'>Ustvarjen nov uporabnik!<br>'" +
+                      ehrId + "'.</span><br><br>");
+                      
+                      if (stPacienta == 0) {
+                      	meritve(ehrId, "2003-03-16T07:17", "175", "70", "37", "110", "75", "98");
+                      } else if (stPacienta == 1) {
+                      	meritve(ehrId, "2004-02-15T07:20", "130", "45", "36.5", "115", "70", "97");
+                      } else if (stPacienta == 2) {
+                      	meritve(ehrId, "2005-05-07T07:15", "190", "150", "36", "120", "90", "98");
+                      }
+                      
+                      valueEHRID[stPacienta] = ehrId;
+	                  return ehrId;
+	                }
+	            },
+	            error: function(err) {
+	            	$("#obvestilo-id").append("<span class='obvestilo label " +
+                "label-danger fade-in'>Napaka '" +
+                JSON.parse(err.responseText).userMessage + "'!</span><br>");
+                return;
+	            }
+	        });
+	    }
+	});
   return ehrId;
 }
 
+function narisiGraf(bmi)  {
+	$("#fillgauge1").empty();
 
-// TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
+    var options = liquidFillGaugeDefaultSettings();
+    options.displayPercent = false;
+    options.circleColor = "#B3F341";
+    
+    if (bmi < 18.5) {
+    	options.waveColor = "#C0C0C0";
+    } else if(bmi < 25) {
+    	options.waveColor = "#B3F341";
+    } else if(bmi < 30) {
+    	options.waveColor = "#FAF63B";
+    } else if(bmi < 40) {
+    	options.waveColor = "#FF9F1F";
+    } else {
+    	options.waveColor = "#FC0012";
+    }
+    
+    var gauge1 = loadLiquidFillGauge("fillgauge1", bmi, options);
+}
+
+function meritve(ehrId, datumInUra, telesnaVisina, telesnaTeza, telesnaTemperatura, sistolicniKrvniTlak, diastolicniKrvniTlak, nasicenostKrviSKisikom) {
+    var sessionId = getSessionId();
+    
+    $.ajaxSetup({
+	    headers: {"Ehr-Session": sessionId}
+	});
+	var podatki = {
+	    "ctx/language": "en",
+	    "ctx/territory": "SI",
+	    "ctx/time": datumInUra,
+	    "vital_signs/height_length/any_event/body_height_length": telesnaVisina,
+	    "vital_signs/body_weight/any_event/body_weight": telesnaTeza,
+	   	"vital_signs/body_temperature/any_event/temperature|magnitude": telesnaTemperatura,
+	    "vital_signs/body_temperature/any_event/temperature|unit": "°C",
+	    "vital_signs/blood_pressure/any_event/systolic": sistolicniKrvniTlak,
+	    "vital_signs/blood_pressure/any_event/diastolic": diastolicniKrvniTlak,
+	    "vital_signs/indirect_oximetry:0/spo2|numerator": nasicenostKrviSKisikom
+	};
+	var parametriZahteve = {
+	    ehrId: ehrId,
+	    templateId: 'Vital Signs',
+	    format: 'FLAT'
+	};
+	$.ajax({
+	    url: baseUrl + "/composition?" + $.param(parametriZahteve),
+	    type: 'POST',
+	    contentType: 'application/json',
+	    data: JSON.stringify(podatki),
+	    success: function (res) {
+	        return;
+	    },
+	    error: function(err) {
+	    	$("#obvestilo-id").append(
+        "<span class='obvestilo label label-danger fade-in'>Napaka '" +
+        JSON.parse(err.responseText).userMessage + "'!</span><br>");
+	    }
+	});
+    
+}
+
+if (first_open) {
+	generirajPodatke1to3();
+	first_open = false;
+}
